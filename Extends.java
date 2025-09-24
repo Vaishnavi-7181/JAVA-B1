b8083deb6acaf24f3f594c1a025e92dba63ac49a
Vaishnavi-7181 <vaishnavi.2403164205@vcet.edu.in>
2025-09-24 09:48:59 +0530

    Add files via upload

---
+++ Extends.java
@@ -0,0 +1,19 @@
+class Extends {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound(); // inherited method
+        d.bark();  // own method
+    }
+}
